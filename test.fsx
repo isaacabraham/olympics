0a19a4117b0a11c5a4920a591e3a790e6afd25f2
Isaac Abraham <isaac.abraham@gmail.com>
2021-08-03 19:13:18 +0200

    last changes

--- test.fsx
+++ test.fsx
@@ -4,8 +4,14 @@ type Event = Event of string
 type FullName = FullName of string
 type Country = Country of string
 type Gender = Male | Female
-
 type Season = Summer | Winter
+
+type Athlete =
+    {
+        Name : FullName option
+        Country : Country option
+        Gender : Gender
+    }
 
 type GamesIdentifier =
     {
@@ -14,63 +20,27 @@ type GamesIdentifier =
         Year : int
     }
 
-type GymnasticsDiscipline =
-    | ArtisticG
-
-type SportDu =
-    | Gymnastics of GymnasticsDiscipline
-    | Athletics
-
-type GymasticsEventIndividual =
-    | AllAround
-    | AllRound
-
-type GymnasticsEvent =
-    | Individual of GymasticsEventIndividual option
-    | ParallelBars
-
-type AthleticsEvent =
-    | Athletics of string
-
-type AllEvents =
-    | Athletics of AthleticsEvent
-    | Gymnastics of GymnasticsEvent
-
-type SportRecord =
+type EventCategory =
     {
         Sport : Sport
         Discipline : Discipline
-        Event : AllEvents
+        Event : Event
     }
 
-let sr =
-    {
-        Sport = Sport "Gymnastics"
-        Discipline = Discipline "Artistic G."
-        Event = Gymnastics (Individual (Some AllRound))
-    }
-
-match sr.Event with
-| Gymnastics (Individual None) ->
-    0
-| Gymnastics (Individual (Some AllRound)) ->
-    10
-| Gymnastics (Individual (Some AllAround)) ->
-    0
-| Gymnastics (ParallelBars _) ->
-    0
-| Athletics _ ->
-    10
+type Medal = Gold | Silver | Bronze
 
 type OlympicResult =
     {
         Id : GamesIdentifier
-
-        Sport : Sport
-        Discipline : Discipline
-        Event : string
-
-        Athlete : FullName
-        Country : Country
-        Gender : Gender
+        EventCategory : EventCategory
+        Athlete : Athlete
+        Medal : Medal
     }
+
+#r "nuget:FSharp.Data"
+
+open FSharp.Data
+
+let [<Literal>] Sample = __SOURCE_DIRECTORY__ + "/summer.csv"
+type Summer = CsvProvider<Sample>
+Summer.GetSample().Rows |> Seq.toArray
